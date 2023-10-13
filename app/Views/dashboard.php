4d28dd3ed6be3faeac929727d2b326a9e8ae72e7
vin0414 <vinmogate@gmail.com>
2023-10-13 17:02:02 +0800

    Add menu

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -402,65 +402,40 @@
 							<ul class="submenu">
 								<li><a href="<?=site_url('stocks')?>">All Stocks</a></li>
 								<li><a href="<?=site_url('monitor-stocks')?>">Monitor Stocks</a></li>
-								<li><a href="<?=site_url('orders')?>">Order Materials</a></li>
 								<li><a href="<?=site_url('add-stocks')?>">Add Stocks</a></li>
 								<li><a href="<?=site_url('Transfer')?>">Transfer Items</a></li>
-								<li><a href="i<?=site_url('dead-stocks')?>">Dead Stocks</a></li>
+								<li><a href="<?=site_url('dead-stocks')?>">Dead Stocks</a></li>
 								<li><a href="<?=site_url('overhaul-stocks')?>">Overhaul Items</a></li>
 							</ul>
 						</li>
 						<li class="dropdown">
 							<a href="javascript:;" class="dropdown-toggle">
-								<span class="micon bi bi-table"></span
-								><span class="mtext">Tables</span>
+                            <i class="micon dw dw-shopping-cart"></i><span class="mtext">Purchasing</span>
 							</a>
 							<ul class="submenu">
-								<li><a href="basic-table.html">Basic Tables</a></li>
-								<li><a href="datatable.html">DataTables</a></li>
-							</ul>
-						</li>
-						<li>
-							<a href="calendar.html" class="dropdown-toggle no-arrow">
-								<span class="micon bi bi-calendar4-week"></span
-								><span class="mtext">Calendar</span>
-							</a>
-						</li>
-						<li class="dropdown">
-							<a href="javascript:;" class="dropdown-toggle">
-								<span class="micon bi bi-archive"></span
-								><span class="mtext"> UI Elements </span>
-							</a>
-							<ul class="submenu">
-								<li><a href="ui-buttons.html">Buttons</a></li>
-								<li><a href="ui-cards.html">Cards</a></li>
-								<li><a href="ui-cards-hover.html">Cards Hover</a></li>
-								<li><a href="ui-modals.html">Modals</a></li>
-								<li><a href="ui-tabs.html">Tabs</a></li>
-								<li>
-									<a href="ui-tooltip-popover.html">Tooltip &amp; Popover</a>
-								</li>
-								<li><a href="ui-sweet-alert.html">Sweet Alert</a></li>
-								<li><a href="ui-notification.html">Notification</a></li>
-								<li><a href="ui-timeline.html">Timeline</a></li>
-								<li><a href="ui-progressbar.html">Progressbar</a></li>
-								<li><a href="ui-typography.html">Typography</a></li>
-								<li><a href="ui-list-group.html">List group</a></li>
-								<li><a href="ui-range-slider.html">Range slider</a></li>
-								<li><a href="ui-carousel.html">Carousel</a></li>
+                                <li><a href="<?=site_url('orders')?>">Order Materials</a></li>
+                                <li><a href="<?=site_url('payment')?>">For Payment</a></li>
+								<li><a href="<?=site_url('list-orders')?>">List Order</a></li>
 							</ul>
 						</li>
 						<li class="dropdown">
 							<a href="javascript:;" class="dropdown-toggle">
-								<span class="micon bi bi-command"></span
-								><span class="mtext">Icons</span>
+                                <i class="micon dw dw-clipboard1"></i><span class="mtext">Receiving</span>
 							</a>
 							<ul class="submenu">
-								<li><a href="bootstrap-icon.html">Bootstrap Icons</a></li>
-								<li><a href="font-awesome.html">FontAwesome Icons</a></li>
-								<li><a href="foundation.html">Foundation Icons</a></li>
-								<li><a href="ionicons.html">Ionicons Icons</a></li>
-								<li><a href="themify.html">Themify Icons</a></li>
-								<li><a href="custom-icon.html">Custom Icons</a></li>
+                                <li><a href="<?=site_url('receiving-item')?>">Receiving Item</a></li>
+                                <li><a href="<?=site_url('storage')?>">Item Storage</a></li>
+                                <li><a href="<?=site_url('packaging')?>">Packaging</a></li>
+                                <li><a href="<?=site_url('shipping')?>">Shipping Items</a></li>
+							</ul>
+						</li>
+						<li class="dropdown">
+							<a href="javascript:;" class="dropdown-toggle">
+                                <i class="micon dw dw-shop"></i><span class="mtext">Suppliers</span>
+							</a>
+							<ul class="submenu">
+                                <li><a href="<?=site_url('list')?>">List of Suppliers</a></li>
+								<li><a href="<?=site_url('add-supplier')?>">Add Supplier</a></li>
 							</ul>
 						</li>
 						<li class="dropdown">
