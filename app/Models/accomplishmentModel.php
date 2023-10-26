d035727395484555b1b6eee7f01ef11d69d9cf57
vin0414 <vinmogate@gmail.com>
2023-10-26 14:33:44 +0800

    add model

---
+++ app/Models/accomplishmentModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class accomplishmentModel extends Model
+{
+    protected $table      = 'tblaccomplishment';
+    protected $primaryKey = 'accomplishmentID';
+
+    protected $useAutoIncrement  = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $userSoftDelete = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['repairID', 'Workers','File','DateCreated'];
+
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+    
+    
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
