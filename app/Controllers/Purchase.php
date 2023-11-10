0aadf682ff7429d55ae3c59ac6d2e6848a9c8941
vin0414 <vinmogate@gmail.com>
2023-11-10 14:47:29 +0800

    Add new controller

---
+++ app/Controllers/Purchase.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+class Purchase extends BaseController
+{
+    private $db;
+    public function __construct()
+    {
+        helper(['url','form']);
+        $this->db = db_connect();
+    }
+}
