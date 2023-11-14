aff4be7a481693278bb46899c2745f05c36303c7
vin0414 <vinmogate@gmail.com>
2023-11-14 11:03:24 +0800

    add system logs model

---
+++ app/Models/systemLogsModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class systemLogsModel extends Model
+{
+    protected $table      = 'tblsystem_logs';
+    protected $primaryKey = 'systemID';
+
+    protected $useAutoIncrement  = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $userSoftDelete = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['accountID','Date','Activity'];
+
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+    
+    
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
