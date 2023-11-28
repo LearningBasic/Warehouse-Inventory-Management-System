f155333dcca679f5aee6e29b9c6b31e40f74adab
vin0414 <vinmogate@gmail.com>
2023-11-28 17:25:47 +0800

    add badges

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -415,14 +415,14 @@
 						</li>
 						<li class="dropdown">
 							<a href="javascript:;" class="dropdown-toggle">
-                            <i class="micon dw dw-shopping-cart"></i><span class="mtext">Purchasing</span>
+                            <i class="micon dw dw-shopping-cart"></i><span class="mtext">Purchasing</span>&nbsp;<span class="badge badge-pill bg-primary text-white" id="notification">0</span>
 							</a>
 							<ul class="submenu">
                                 <li><a href="<?=site_url('orders')?>">Order Materials</a></li>
                                 <li><a href="<?=site_url('payment')?>">For Payment</a></li>
 								<li><a href="<?=site_url('list-orders')?>">List Order</a></li>
 								<?php if(session()->get('role')=="Administrator"||session()->get('role')=="Editor"){ ?>
-								<li><a href="<?=site_url('approve-orders')?>">For Approval</a></li>
+								<li><a href="<?=site_url('approve-orders')?>">For Approval&nbsp;<span class="badge badge-pill bg-primary text-white" id="notifications">0</span></a></li>
 								<?php } ?>
 							</ul>
 						</li>
@@ -650,7 +650,7 @@
 		<script src="assets/src/plugins/datatables/js/dataTables.responsive.min.js"></script>
 		<script src="assets/src/plugins/datatables/js/responsive.bootstrap4.min.js"></script>
 		<script>
-			$(document).ready(function(){totalItem();totalStocks();totalReserved();totalSupplier();outStock();listSupplier();});
+			$(document).ready(function(){totalItem();totalStocks();totalReserved();totalSupplier();outStock();listSupplier();notify();});
 			function totalItem(){$.ajax({url:"<?=site_url('total-item')?>",method:"GET",success:function(response){$('#allStocks').html(response);}});}
 			function totalStocks(){$.ajax({url:"<?=site_url('total-stocks')?>",method:"GET",success:function(response){$('#totalStocks').html(response);}});}
 			function totalReserved(){$.ajax({url:"<?=site_url('total-reserved')?>",method:"GET",success:function(response){$('#totalReserved').html(response);}});}
@@ -724,6 +724,18 @@
 				/* Instantiate and draw the chart.*/
 				var chart = new google.visualization.PieChart(document.getElementById('chartCategory'));
 				chart.draw(data, options);
+			}
+
+			function notify()
+			{
+				$.ajax({
+					url:"<?=site_url('notification')?>",method:"GET",
+					success:function(response)
+					{
+						$('#notification').html(response);
+						$('#notifications').html(response);
+					}
+				});
 			}
 		</script>
 	</body>
