d5fa89da650bbc259376b77f7df82e35d392210f
vin0414 <vinmogate@gmail.com>
2023-12-01 11:21:21 +0800

    add canvas model

---
+++ app/Models/canvassModel.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class canvassModel extends Model
+{
+    protected $table      = 'tblcanvass_sheet';
+    protected $primaryKey = 'canvassID';
+
+    protected $useAutoIncrement  = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $userSoftDelete = false;
+    protected $protectFields = true;
+    protected $allowedFields = ['OrderNo', 'orderID','Supplier','ContactPerson','ContactNumber','Terms','Warranty','Reference'];
+
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+    
+    
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
