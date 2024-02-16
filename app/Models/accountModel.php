a83a84a6cf45460ae363065b7043a78964944ffb
vin0414 <vinmogate@gmail.com>
2024-02-16 09:42:56 +0800

    upload signatures

--- app/Models/accountModel.php
+++ app/Models/accountModel.php
@@ -14,7 +14,7 @@ class accountModel extends Model
     protected $returnType = 'array';
     protected $userSoftDelete = false;
     protected $protectFields = true;
-    protected $allowedFields = ['username', 'password','Fullname','Email','Status','systemRole','warehouseID','Department','DateCreated'];
+    protected $allowedFields = ['username', 'password','Fullname','Email','Status','systemRole','warehouseID','Department','DateCreated','Signature'];
 
     protected $validationRules = [];
     protected $validationMessages = [];
